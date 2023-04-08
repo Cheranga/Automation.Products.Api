fcfe4304e46312e5f945a7a776e14d672dd83c05
Cheranga Hatangala <cheranga@gmail.com>
2023-04-09 08:43:18 +1000

    chore: adding validation to upsert and get entity methods

--- src/Storage.Table.Helper/ErrorCodes.cs
+++ src/Storage.Table.Helper/ErrorCodes.cs
@@ -1,17 +1,19 @@
 namespace Storage.Table.Helper;
 
-public class ErrorCodes
+public static class ErrorCodes
 {
     public const int UnregisteredTableService = 500;
     public const int TableUnavailable = 501;
     public const int CannotUpsert = 502;
     public const int EntityDoesNotExist = 503;
+    public const int Invalid = 504;
 }
 
-public class ErrorMessages
+public static class ErrorMessages
 {
     public const string UnregisteredTableService = "table service is unregistered for the storage account.";
     public const string TableUnavailable = "table is unavailable";
     public const string CannotUpsert = "data cannot be upserted into the storage table";
     public const string EntityDoesNotExist = "entity does not exist";
+    public const string Invalid = "invalid";
 }
--- src/Storage.Table.Helper/TableService.cs
+++ src/Storage.Table.Helper/TableService.cs
@@ -1,6 +1,8 @@
 using Azure.Data.Tables;
 using LanguageExt;
+using LanguageExt.Common;
 using Microsoft.Extensions.Azure;
+using static LanguageExt.Prelude;
 using static Storage.Table.Helper.AzureTableStorageWrapper;
 
 namespace Storage.Table.Helper;
@@ -39,6 +41,8 @@ internal class TableService : ITableService
     ) where T : ITableEntity =>
         (
             await (
+                from _1 in ValidateEmptyString(category)
+                from _2 in ValidateEmptyString(table)
                 from sc in GetServiceClient(_factory, category)
                 from tc in GetTableClient(sc, table)
                 from op in Upsert(tc, data, token, merge)
@@ -61,6 +65,10 @@ internal class TableService : ITableService
     ) where T : class, ITableEntity =>
         (
             await (
+                from _1 in ValidateEmptyString(category)
+                from _2 in ValidateEmptyString(table)
+                from _3 in ValidateEmptyString(partitionKey)
+                from _4 in ValidateEmptyString(rowKey)
                 from sc in GetServiceClient(_factory, category)
                 from tc in GetTableClient(sc, table)
                 from op in GetEntityAsync<T>(tc, partitionKey.ToUpper(), rowKey.ToUpper(), token)
@@ -73,4 +81,37 @@ internal class TableService : ITableService
                     TableOperationError.New(err.Code, err.Message, err.ToException())
                 )
         );
+
+    private static Eff<Unit> ValidateEmptyString(string s) =>
+        from _1 in guardnot(
+                string.IsNullOrWhiteSpace(s),
+                Error.New(ErrorCodes.Invalid, ErrorMessages.Invalid)
+            )
+            .ToEff()
+        select unit;
+
+    private static Eff<Unit> ValidateEmptyString(
+        string category,
+        string table,
+        string partitionKey,
+        string rowKey
+    ) =>
+        from _1 in guardnot(
+                string.IsNullOrWhiteSpace(category),
+                Error.New("category cannot be null or empty")
+            )
+            .ToEff()
+        from _2 in guardnot(
+            string.IsNullOrWhiteSpace(table),
+            Error.New("table cannot be null or empty")
+        )
+        from _3 in guardnot(
+            string.IsNullOrWhiteSpace(partitionKey),
+            Error.New("partition key cannot be null or empty")
+        )
+        from _4 in guardnot(
+            string.IsNullOrWhiteSpace(rowKey),
+            Error.New("row key cannot be null or empty")
+        )
+        select unit;
 }
