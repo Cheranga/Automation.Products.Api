cd22473fb926958a20ccea5430a32de52fcf3254
Cheranga Hatangala <cheranga@gmail.com>
2023-04-15 21:19:31 +1000

    chore: using similar approach as in the registration when updating location

--- src/Demo.MiniProducts.Api/Features/ChangeLocation/Service.cs
+++ src/Demo.MiniProducts.Api/Features/ChangeLocation/Service.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using Azure.Storage.Table.Wrapper.Commands;
 using Azure.Storage.Table.Wrapper.Queries;
+using Demo.MiniProducts.Api.Core;
 using Demo.MiniProducts.Api.DataAccess;
 using Demo.MiniProducts.Api.Extensions;
 using Demo.MiniProducts.Api.Features.RegisterProduct;
@@ -9,6 +10,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Storage.Queue.Helper;
 using static Microsoft.AspNetCore.Http.TypedResults;
+using static Demo.MiniProducts.Api.Extensions.ApiOperationExtensions;
 
 namespace Demo.MiniProducts.Api.Features.ChangeLocation;
 
@@ -23,65 +25,31 @@ public static class Service
         [FromServices] RegisterProductSettings registerSettings,
         [FromServices] IQueueService queueService,
         [FromServices] IQueryService queryService,
-        [FromServices] ICommandService commandService
+        [FromServices] ICommandService commandService,
+        CancellationToken token = new()
     )
     {
-        var validationResult = await validator.ValidateAsync(request);
-        if (!validationResult.IsValid)
-            return validationResult.ToValidationErrorResponse();
+        var validateOperation = await Validate(request, validator, token);
+        if (validateOperation.Operation is ApiOperation.ApiValidationFailureOperation vf)
+            return vf.ValidationResult.ToValidationErrorResponse();
 
-        return await UpdateLocation(
-            request,
-            updateSettings,
-            registerSettings,
-            queueService,
-            queryService,
-            commandService,
-            new CancellationToken()
-        );
-    }
-
-    private static async Task<
-        Results<ValidationProblem, ProblemHttpResult, NotFound, NoContent>
-    > UpdateLocation(
-        ChangeLocationRequest request,
-        UpdateProductSettings updatedSettings,
-        RegisterProductSettings registerSettings,
-        IQueueService queueService,
-        IQueryService queryService,
-        ICommandService tableService,
-        CancellationToken token
-    )
-    {
-        var getProductOperation = await queryService.GetEntityAsync<ProductDataModel>(
+        var getProductOperation = await GetEntity<ProductDataModel>(
             registerSettings.Category,
             registerSettings.Table,
             request.Category.ToUpper(),
             request.Id.ToUpper(),
+            queryService,
             token
         );
+        if (getProductOperation.Operation is ApiOperation.ApiFailedOperation gf)
+            return gf.ToErrorResponse();
 
-        if (getProductOperation.Response is QueryResult.QueryFailedResult f)
-        {
-            return Problem(
-                new ProblemDetails
-                {
-                    Type = "Error",
-                    Title = f.ErrorCode.ToString(),
-                    Detail = f.ErrorMessage,
-                    Status = StatusCodes.Status500InternalServerError
-                }
-            );
-        }
-
-        if (getProductOperation.Response is QueryResult.EmptyResult)
-        {
+        if (getProductOperation.Operation is ApiOperation.ApiSuccessfulOperation)
             return NotFound();
-        }
 
         var product = (
-            getProductOperation.Response as QueryResult.SingleResult<ProductDataModel>
-        )!.Entity;
+            getProductOperation.Operation as ApiOperation.ApiSuccessfulOperation<ProductDataModel>
+        )!.Data;
 
         var updatedProduct = ProductDataModel.New(
             product.Category,
@@ -89,35 +57,57 @@ public static class Service
             product.Name,
             request.LocationCode
         );
-        await UpdateLocation(updatedProduct, registerSettings, tableService, token);
+        var updateOperation = await UpdateLocation(
+            updatedProduct,
+            registerSettings,
+            commandService,
+            token
+        );
+        if (updateOperation.Operation is ApiOperation.ApiFailedOperation f)
+            return f.ToErrorResponse();
 
-        await PublishLocationChangedEvent(
+        var publishOperation = await PublishLocationChangedEvent(
             product.LocationCode,
             request,
-            updatedSettings,
+            updateSettings,
             queueService,
             token
         );
 
+        if (publishOperation.Operation is ApiOperation.ApiFailedOperation pf)
+            return pf.ToErrorResponse();
+
         return NoContent();
     }
 
-    private static async Task UpdateLocation(
+    private static async Task<
+        ApiOperationResult<ApiOperation.ApiFailedOperation, ApiOperation.ApiSuccessfulOperation>
+    > UpdateLocation(
         ProductDataModel product,
         RegisterProductSettings registerSettings,
         ICommandService commandService,
         CancellationToken token
     )
     {
-        await commandService.UpdateAsync(
+        var op = await commandService.UpdateAsync(
             registerSettings.Category,
             registerSettings.Table,
             product,
             token
         );
+
+        return op.Operation switch
+        {
+            CommandOperation.CommandFailedOperation f
+                => ApiOperation.Fail(f.ErrorCode, f.ErrorMessage, f.Exception),
+            CommandOperation.CommandSuccessOperation => ApiOperation.Success(),
+            _ => throw new NotSupportedException()
+        };
     }
 
-    private static async Task PublishLocationChangedEvent(
+    private static async Task<
+        ApiOperationResult<ApiOperation.ApiFailedOperation, ApiOperation.ApiSuccessfulOperation>
+    > PublishLocationChangedEvent(
         string previousLocationCode,
         ChangeLocationRequest request,
         UpdateProductSettings updatedSettings,
@@ -131,16 +121,18 @@ public static class Service
             request.LocationCode
         );
 
-        await queueService.PublishAsync(
+        var op = await queueService.PublishAsync(
             updatedSettings.Category,
             token,
             (updatedSettings.Queue, () => JsonSerializer.Serialize(@event))
         );
-    }
 
-    private static ProductDataModel ToDataModel(
-        this ChangeLocationRequest request,
-        string name,
-        string locationCode
-    ) => ProductDataModel.New(request.Category, request.Id, name, locationCode);
+        return op switch
+        {
+            QueueOperation.FailedOperation f
+                => ApiOperation.Fail(f.Error.Code, f.Error.Message, f.Error.ToException()),
+            QueueOperation.SuccessOperation => ApiOperation.Success(),
+            _ => throw new NotSupportedException()
+        };
+    }
 }
