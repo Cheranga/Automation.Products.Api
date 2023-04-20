9a93addcbe74897cae8c06f8655683b0b4194e2b
Cheranga <cheranga@gmail.com>
2023-04-20 20:10:15 +1000

    chore: adding a readme to integration tests [ci]

---
+++ tests/Demo.MiniProducts.Automation.Tests/RegisterProduct/EndpointTests.cs
@@ -0,0 +1,17 @@
+namespace Demo.MiniProducts.Automation.Tests.RegisterProduct;
+
+public class EndpointTests
+{
+    private readonly HttpClient _httpClient;
+
+    public EndpointTests()
+    {
+        _httpClient = new HttpClient();
+    }
+    
+    [Fact]
+    public async Task Test()
+    {
+        
+    }
+}
